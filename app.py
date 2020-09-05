54a9c0451e7b24a4a54239cce1db5cb71d7b9bce
vishesh342 <56043783+vishesh342@users.noreply.github.com>
2020-09-05 16:36:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,12 +18,12 @@ st.image(image,width=800)
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
 data=pd.read_csv('Reviews.csv')
-
+data.drop(['Id','ProductId','UserId','ProfileName','Time','Text','HelpfulnessNumerator','HelpfulnessDenominator'],axis=1,inplace=True)
 data.dropna(axis=0,inplace=True)
 data['Sentiment']=data['Score'].apply(lambda Score: 'Positive' if Score>3 else('Negative' if Score<3 else "Neutral"))
 index=data[data['Sentiment']==0].index
 data.drop(index=index,axis=0,inplace=True)
-X=data.iloc[:,8:9]
+X=data.iloc[:,1]
 y=data['Sentiment']
 xtrain,xtest,ytrain,ytest=train_test_split(X,y,test_size=0.3,random_state=0)
 cv = TfidfVectorizer()
